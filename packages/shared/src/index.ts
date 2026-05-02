ae7f589ba5256dc85d8683b39422b8ff73a4e50d
Shawn GB <god.shawn@gmail.com>
2026-05-02 12:50:29 +0200

    refactor(shared): use type aliases, add ApiError and ApiResponse

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,13 @@
-export interface HelloMessage {
+export type HelloMessage = {
   message: string
 }
+
+export type ApiError = {
+  message: string
+  code: string
+  statusCode: number
+}
+
+export type ApiResponse<T> =
+  | { data: T; error: null }
+  | { data: null; error: ApiError }
